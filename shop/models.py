e725529a47dd2cd5e0d92777d5203ed91468bc78
mkrzyszton <michal.krzyszton@microsoft.wsei.edu.pl>
2022-06-12 14:09:18 +0200

    Cart update

--- shop/models.py
+++ shop/models.py
@@ -56,3 +56,8 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
--- shop/views.py
+++ shop/views.py
@@ -16,10 +16,11 @@ def products(request):
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
     context = {'items': items, 'order': order}
     return render(request, 'shop/cart.html', context)
