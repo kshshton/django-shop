29c6d21e2172673085dfc1531f68aca768fa0f58
kshshton <michkrzyszton@gmail.com>
2022-08-06 02:49:45 +0200

    Add login, logout

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .utils import cookieCart, cartData, guestOrder
 from .forms import CreateUserForm
@@ -22,6 +24,7 @@ def products(request):
     context = {'products': products, 'cartItems': cartItems}
     return render(request, 'shop/products.html', context)
 
+
 def cart(request):
     data = cartData(request)
 
@@ -32,9 +35,26 @@ def cart(request):
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'shop/cart.html', context)
 
+
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Incorrect data')
+
     context = {}
     return render(request, 'shop/login.html', context)
+
+
+def logout(request):
+    logout(request)
+    return redirect('login')
 
 
 def register(request):
@@ -43,6 +63,8 @@ def register(request):
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
+            user = form.cleaned_data.get('username')
+            messages.success(request, f'Account {user} has been created')
             form.save()
 
     context = {'form': form}
